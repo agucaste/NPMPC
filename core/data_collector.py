8d868517f8c68112fcb69343cdbedd7f92cb6c5c
Agustin Castellano <agustincastellano17@gmail.com>
2025-09-11 17:26:09 -0400

    implemented collect_data method

--- core/data_collector.py
+++ core/data_collector.py
@@ -21,7 +21,7 @@ class DataCollector:
         }
         assert mpc.settings.n_horizon <= self.steps
 
-    def run_simulation(self, x0: np.ndarray):
+    def run_trajectory(self, x0: np.ndarray):
         self.simulator.x0 = x0
         self.mpc.x0 = x0
         self.mpc.set_initial_guess()
@@ -45,7 +45,37 @@ class DataCollector:
         self.data['dx'].append(np.array(dx_hist).squeeze())
         self.data['u'].append(np.array(u_hist).squeeze())
 
+    def collect_data(self, num_trajectories: int, lb: np.ndarray | float, ub: np.ndarray | float, method: str = 'random'):
+        """
+        Collect data by running trajectories from random initial conditions.
+        Args:
+            num_trajectories: number of trajectories to collect.
+            lb: lower bound for initial condition.
+            ub: upper bound for initial condition.
+            method: 'random' or 'grid' for sampling initial conditions.
+        """
+        # Dimension of state.
+        n = self.model.x.shape[0]
+        # Cast bounds to arrays if needed.
+        lb = lb * np.ones(n) if isinstance(lb, (float, int)) else lb
+        ub = ub * np.ones(n) if isinstance(ub, (float, int)) else ub
 
+        assert method in ['random', 'grid']    
+        if method == 'random':
+            # Uniform sampling over [lb, ub]
+            X0 = np.random.uniform(lb, ub, size=(num_trajectories, n))            
+        elif method == 'grid':
+            # Creates a uniform grid of initial conditions.
+            axs = [np.linspace(lb[i], ub[i], int(np.ceil(num_trajectories**(1/len(lb))))) for i in range(len(lb))]
+            grid = np.meshgrid(*axs)
+            X0 = np.c_[tuple(g.ravel() for g in grid)]
+                        
+        for t in range(num_trajectories):
+            x0 = X0[t].reshape(-1,1)
+            self.run_trajectory(x0)
+
+        return self.get_data()
+    
     def get_velocity(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
         return self.model._rhs_fun(x, u, [], [], [], [])
         self.data['reference'].append(reference)
@@ -58,18 +88,26 @@ if __name__ == "__main__":
     collector = DataCollector(model, mpc, simulator)
 
 
-    X0 = np.meshgrid(
-        np.linspace(-2, 2, 4),
-        np.linspace(-2, 2, 4)
-    )
-    X0 = np.c_[tuple(X.ravel() for X in X0)]
-    T = X0.shape[0]
-    for t in range(T):
-        print(f' collecting trajectory {t+1}/{T}')
-        x0 = np.random.uniform(low=-2, high=2, size=(2,1))    
-        collector.run_simulation(x0)
+
+    data = collector.collect_data(num_trajectories=7**2, lb=-2, ub=2, method='grid')
+    print(data['x'])
     
-    data = collector.get_data()
+
+    # X0 = np.meshgrid(
+    #     np.linspace(-2, 2, 5),
+    #     np.linspace(-2, 2, 5)
+    # )
+    # X0 = np.c_[tuple(X.ravel() for X in X0)]
+    # print(f"X0 shape: {X0.shape}")
+    # T = X0.shape[0]
+    # T = 10
+    # for t in range(T):
+    #     print(f' collecting trajectory {t+1}/{T}')
+    #     x0 = X0[t].reshape(-1,1)
+    #     print(f"x0 = {x0}")
+    #     collector.run_trajectory(x0)
+    
+    # data = collector.get_data()
 
     print(f"Collected data keys: {list(data.keys())}")
     for key, value in data.items():
