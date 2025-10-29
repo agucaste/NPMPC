90e9ec128fd3d224655b6650e3460295ea5ee60c
agucaste <agustincastellano17@gmail.com>
2025-10-29 10:37:27 -0400

    fixed return method for tarjectory plotter

--- core/trajectory_plotter.py
+++ core/trajectory_plotter.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # for env in valid_environments:
     for env in ['pendulum']:
         cfgs = get_default_kwargs_yaml(algo='', env_id=env)
-        model, mpc_t, mpc_h, simulator = constructor(env, cfgs)
+        model, mpcs, simulator = constructor(env, cfgs)
 
         n = model.x.shape[0]  # state dimension
         G = 3  # grid points per dimension
@@ -22,9 +22,11 @@ if __name__ == "__main__":
         X0 = np.column_stack([g.ravel() for g in grid]).reshape(M, n, 1)
         sampler = Sampler(X0=X0)
 
+        mpc_t = mpcs[0]  # full horizon MPC
         evaluator = Evaluator(mpc=mpc_t)
-        stats = evaluator.evaluate(n, mpc_t, simulator, cfgs, sampler, M)
-        stats = evaluator.evaluate(n, mpc_h, simulator, cfgs, sampler, M)
+        for mpc in mpcs:
+            stats = evaluator.evaluate(n, mpc, simulator, cfgs, sampler, M)
+            stats = evaluator.evaluate(n, mpc, simulator, cfgs, sampler, M)
 
         evaluator.plot_trajectories(filename=f'trajectories_{env}_G_{G}_M_{M}.pdf')
         evaluator.plot_boxplots(filename=f'bp_only_mpc_{env}_M_{M}.pdf')
